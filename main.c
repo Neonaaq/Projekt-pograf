13b2893003c3b9366f141faae6a6e9f792837bb1
Enyo467 <edyta.kozlowska1@onet.pl>
2025-04-27 14:54:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -32,33 +32,34 @@ int main(int argc, char *argv[])
 
     // Test, czy macierz dobrze ma zapisane połączenia np dla 0:
 
-    // for(int i=0; i<lwezlow; i++) {
-    //     if(macierz[0][i]==1)
-    //         printf("%d\n", i);
-    // }
+     //for(int i=0; i<lwezlow; i++) {
+     //    if(macierz[0][i]==1)
+     //        printf("%d\n", i);
+     //}
 
     //  Wizualizacja grafu:
     
-    // int arow = 0, acol = 0;
-    // for(int i=0; i<lwezlow; i++) {
-    //     while(p[i].row!=arow) {
-    //         arow++;
-    //         acol=0;
-    //         printf("\n");
-    //     }
-    //     while(p[i].column!=acol) {
-    //         acol++;
-    //         printf("   ");
-    //     }
-    //     acol++;
-    //     if(i>=0 && i <10) printf(" %d ", i);
-    //     if(i>=10 && i <100) printf(" %d", i);
-    //     if(i>=100) printf("%d", i);
-    // }
-
+     //int arow = 0, acol = 0;
+     //for(int i=0; i<lwezlow; i++) {
+     //    while(p[i].row!=arow) {
+     //        arow++;
+     //        acol=0;
+     //        printf("\n");
+     //    }
+     //    while(p[i].column!=acol) {
+     //        acol++;
+     //        printf("   ");
+     //    }
+     //    acol++;
+     //    if(i>=0 && i <10) printf(" %d ", i);
+     //    if(i>=10 && i <100) printf(" %d", i);
+     //    if(i>=100) printf("%d", i);
+     //}
+    //printf("\n");
 
 
     // Sprawdzenie, czy dobrze zostaly podzielone
+
     //FILE *out = fopen(flaga.output_filename, flaga.file_mode == TEXT ? "w" : "wb");
     //for (int i = 0; i < lwezlow; i++) {
     //    fprintf(out, "%d %d\n", i, p[i].part + 1);
