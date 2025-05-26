5ce11ffb6b15d23f7a7b4fdfe443b51d8b1dadc8
Enyo467 <edyta.kozlowska1@onet.pl>
2025-05-26 22:01:39 +0200

    Delete out/production/Projekt-pograf directory

--- out/production/Projekt-pograf/podziel_graf.h
+++
@@ -1,9 +0,0 @@
-#ifndef PODZIEL_GRAF_H
-#define PODZIEL_GRAF_H
-
-#include "odczytajgraf.h"
-#include "args.h"
-
-void podziel_graf(Komorka *p, int **macierz, int lwezlow, struct Flagi *flaga);
-
-#endif //PODZIEL_GRAF_H
--- out/production/Projekt-pograf/wypiszgraf.h
+++
@@ -1,9 +0,0 @@
-#ifndef WYPISZGRAF_H
-#define WYPISZGRAF_H
-
-#include <stdio.h>
-#include "args.h"
-
-void wypiszgraf(FILE *plik, int **macierz, int lwezlow, struct Flagi *flaga);
-
-#endif
